c43a3ebcff8eb5c0253173e4a66f5f4833521076
mdrake <matthew.drake@22squared.com>
2014-03-03 16:01:40 -0500

    Plugin isn't usable yet - need to fix expectations regarding dir generation

---
+++ index.js
@@ -0,0 +1,118 @@
+'use strict';
+var path = require('path');
+var gutil = require('gulp-util');
+var through = require('through2');
+var Connection = require('ssh2');
+
+
+module.exports = function (options) {
+	if (options.host === undefined) {
+		throw new gutil.PluginError('gulp-sftp', '`host` required.');
+	}
+
+	var fileCount = 0;
+	var remotePath = options.remotePath || '';
+  var localPath = options.localPath || '';
+  var logFiles = options.logFiles === false ? false : true;
+
+	delete options.remotePath;
+  delete options.localPath;
+  delete options.logFiles;
+  
+	return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			this.push(file);
+			return cb();
+		}
+
+		if (file.isStream()) {
+			this.emit('error', new gutil.PluginError('gulp-sftp', 'Streaming not supported'));
+			return cb();
+		}
+
+		// have to create a new connection for each file otherwise they conflict
+		var relativePath = file.path.replace(file.cwd + '/', '');
+		var fileBase = file.base?path.resolve(file.base) : file.cwd;
+		
+		var localRelativePath = file.path.replace(path.join(fileBase, localPath), '');
+		var finalRemotePath = path.join('/', remotePath, localRelativePath);
+		
+		//MDRAKE: TODO pool requests into single connection
+		var c = new Connection();
+		c.on('ready', function() {
+			
+			c.sftp(function(err, sftp) {
+				if (err)
+					throw err;
+				sftp.on('end', function() {
+					console.log('SFTP :: SFTP session closed');
+				});
+				
+				//MDRAKE: TODO - create directories as needed
+				/*var dirname=path.dirname(finalRemotePath);
+					sftp.opendir(dirname,function readdir(err,handle){
+					if(err){
+					
+					}
+				});*/
+				var stream = sftp.createWriteStream(finalRemotePath,{ flags: 'w',
+					  encoding: null,
+					  mode: '0666',
+					  autoClose: true
+					});
+				//stream.write();
+				
+				stream.on('close', function(err) {
+					console.log('FINISH');
+					if(err)
+					  this.emit('error', new gutil.PluginError('gulp-sftp', err));
+					else{
+						if (logFiles) {
+					          gutil.log('gulp-sftp:', gutil.colors.green('Uploaded: ') + 
+					                                 relativePath +
+					                                 gutil.colors.green(' => ') + 
+					                                 finalRemotePath);
+					        }
+
+						fileCount++;
+					}
+					sftp.end();
+					return c.end();
+				});
+				stream.end(file.contents);
+				
+			});
+		});
+		c.on('error', function(err) {			
+			this.emit('error', new gutil.PluginError('gulp-sftp', err));
+			return cb(err);
+		});
+		c.on('end', function() {
+			console.log('Connection :: end');
+		});
+		c.on('close', function(had_error) {
+			return cb(had_error);
+		});
+		c.connect({
+			host : options.host,
+			port : options.port,
+			username : options.user,
+			password : options.pass
+		});
+		
+		
+		
+		
+
+
+		this.push(file);
+	}, function (cb) {
+		if (fileCount > 0) {
+			gutil.log('gulp-sftp:', gutil.colors.green(fileCount, fileCount === 1 ? 'file' : 'files', 'uploaded successfully'));
+		} else {
+			gutil.log('gulp-sftp:', gutil.colors.yellow('No files uploaded'));
+		}
+
+		cb();
+	});
+};
---
+++ readme.md
@@ -0,0 +1,90 @@
+# [gulp](https://github.com/wearefractal/gulp)-ftp [![Build Status](https://secure.travis-ci.org/sindresorhus/gulp-ftp.png?branch=master)](http://travis-ci.org/sindresorhus/gulp-ftp)
+
+> Upload files to via SSH
+
+Useful for uploading and deploying things.
+
+
+## Install
+
+Install with [npm](https://npmjs.org/package/gulp-sftp)
+
+```
+npm install --save-dev gulp-sftp
+```
+
+
+## Example
+
+```js
+var gulp = require('gulp');
+var sftp = require('gulp-sftp');
+
+gulp.task('default', function () {
+	gulp.src('src/*')
+		.pipe(sftp({
+			host: 'website.com',
+			user: 'johndoe',
+			pass: '1234'
+		}));
+});
+```
+
+
+## API
+
+### sftp(options)
+
+#### options.host
+
+*Required*  
+Type: `String`
+
+#### options.port
+
+Type: `Number`  
+Default: `22`
+
+#### options.user
+
+Type: `String`  
+Default: `'anonymous'`
+
+#### options.pass
+
+Type: `String`  
+Default: `'@anonymous'`
+
+#### options.remotePath
+
+Type: `String`  
+Default: `'/'`
+
+The remote path to upload too.
+
+#### options.localPath
+
+Type: `String`
+Default `'.'`
+
+The local path to upload from.
+This is useful for example when you use
+
+```javascript
+gulp.src('_public/js/*').pipe({remotePath: '/www/some/path'});
+```
+
+And you want the contents of `_public/js` to be uploaded to 
+`/www/some/path`, but not `_public/js` itself. Then you just set
+`localPath` to `_public/js` and you are done.
+
+#### options.logFiles
+
+Type: `Boolean`
+Default: `true`
+
+Logging of files as they are uploaded. If set to false, you will only see a message when all files finished.
+
+## License
+
+MIT © [Sindre Sorhus](http://sindresorhus.com)
