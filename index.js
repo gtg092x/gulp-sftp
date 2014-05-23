45290709a600366eea74614115c1d0de8180ebb1
gtg092x <mdrake@mediadrake.com>
2014-05-23 16:19:05 -0700

    giving the wrong % values

--- index.js
+++ index.js
@@ -270,7 +270,7 @@ module.exports = function (options) {
                     uploadedBytes+=highWaterMark;
                     var p = Math.round((uploadedBytes/size)*100);
                     p = Math.min(100,p);
-                    gutil.log('gulp-sftp:',finalRemotePath,"uploaded",uploadedBytes+"kb");
+                    gutil.log('gulp-sftp:',finalRemotePath,"uploaded",(uploadedBytes/1000)+"kb");
                 });
 
 
